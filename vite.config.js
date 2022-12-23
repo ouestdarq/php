b8d7687b773923932166278530d12ba1cc691275
proxymurder <30883472+proxymurder@users.noreply.github.com>
2022-12-23 17:12:57 -0600

    working vite-plugin-smallstep, commonJS module, gotta check that out but for now it works

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,16 @@
 import { defineConfig, loadEnv } from 'vite';
-
-import fs from 'fs';
+import laravel from 'laravel-vite-plugin';
+import smallstep from 'vite-plugin-smallstep';
 import path from 'path';
 
-import laravel from 'laravel-vite-plugin';
-import restart from 'vite-plugin-restart';
-
-async function __https({ crt, key }) {
-    let https = null;
-    try {
-        https = {
-            cert: fs.readFileSync(crt),
-            key: fs.readFileSync(key),
-        };
-    } catch (err) {
-        await __https({ crt: crt, key: key });
-    }
-    return https;
-}
-
 export default defineConfig(async ({ mode }) => {
-    process.env = {
-        ...process.env,
-        ...loadEnv(mode, process.cwd(), 'VITE_SERVER'),
-    };
-
-    const https = await __https({
-        crt: process.env.VITE_SERVER_CRT,
-        key: process.env.VITE_SERVER_KEY,
-    });
-
     return {
         server: {
             host: true,
             port: 5173,
-            https: https,
             hmr: {
-                host: process.env.VITE_SERVER_HMR,
-                clientPort: 443,
+                host: process.env.VITE_HMR_HOST,
+                clientPort: process.env.VITE_HMR_CLIENTPORT,
             },
         },
         resolve: {
@@ -47,14 +20,12 @@ export default defineConfig(async ({ mode }) => {
             },
         },
         plugins: [
-            restart({
-                restart: [
-                    path.relative(__dirname, process.env.VITE_SERVER_CRT),
-                ],
-            }),
             laravel({
                 input: ['./resources/js/main.js'],
                 refresh: true,
+            }),
+            smallstep({
+                steppath: process.env.VITE_STEPPATH,
             }),
         ],
     };
